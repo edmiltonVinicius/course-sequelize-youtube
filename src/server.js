97b51a2636a02c83ac0a2beeb6701576d154c233
Edmilton Pansanato <epansanato@mtsolucoes.com.br>
2021-07-01 00:40:59 -0300

    Enable cookie-parser and cors and Add routers

--- src/server.js
+++ src/server.js
@@ -5,9 +5,11 @@ const port = process.env.PORT || 8080
 
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import routes  from './routes/routes.js'
 
-app.get('/', (request, response) => {
-    return response.status(200).send('foi mano!!')
-})
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+app.use(routes)
 
 app.listen(port, () => console.log('Server running in port ' + port))
