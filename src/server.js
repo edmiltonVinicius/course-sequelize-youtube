c817bf01f3132e61f0ac9ad44b1239a8017f7de8
Edmilton Pansanato <epansanato@mtsolucoes.com.br>
2021-07-01 00:35:24 -0300

    Add cookier-parser and cors

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 8080
 
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 app.get('/', (request, response) => {
     return response.status(200).send('foi mano!!')
 })
